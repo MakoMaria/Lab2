eda8576d976a8890d2555c48ee129c884d3c7a18
MakoMaria <91057694+MakoMaria@users.noreply.github.com>
2023-03-07 23:36:27 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,42 +1,45 @@
 #include <iostream>
-#include <list>
-#include <algorithm>
-
+#include <stack>
+#include <string>
 using namespace std;
 
-void bubbleSort(list<string>& mylist) {
-    int n = mylist.size();
-
-    for (int i = 0; i < n - 1; i++) {
-        auto it1 = mylist.begin();
-        auto it2 = next(it1, 1);
-
-        for (int j = 0; j < n - i - 1; j++) {
-            // Convert to integers and compare
-            int num1 = stoi(it1->substr(0, it1->find(" ")));
-            int num2 = stoi(it2->substr(0, it2->find(" ")));
-
-            if (num1 > num2) {
-                swap(*it1, *it2);
+int main() {
+    stack <char> st;
+    string line;
+    cin >> line;
+    for(char& c : line) {
+        if (c == '(' || c == '{' || c == '[') {
+            st.push(c);
+        } else {
+            switch (st.top()) { //
+                case '(':
+                    if (c == ')'){
+                        st.pop();
+                    } else {
+                        cout << "no" << endl;
+                        return 0;
+                    }
+                    break;
+                case '[':
+                    if (c == ']'){
+                        st.pop();
+                    } else {
+                        cout << "no" << endl;
+                        return 0;
+                    }
+                    break;
+                case '{':
+                    if (c == '}'){
+                        st.pop();
+                    } else {
+                        cout << "no" << endl;
+                        return 0;
+                    }
+                    break;
             }
-
-            advance(it1, 1);
-            advance(it2, 1);
         }
     }
+    cout << "yes" << endl;
+    return 0;
 }
 
-
-int main() {
-    list<string> listok;
-    string line;
-    while (getline(cin, line)) {
-//        if (line == "end") break;
-        listok.push_back(line);
-    }
-    bubbleSort(listok);
-
-    for (auto i : listok) {
-        cout << i << " ";
-    }
-}
